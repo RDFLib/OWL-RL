8cf1489c794863ce3a43f2b9b36604e32e6c5a5f
wrobell <wrobell@pld-linux.org>
2018-10-15 23:12:05 +0100

    Starting to implement some basic tests to allow removal of `LiteralProxies` class in near future.

---
+++ tests/test_datatypes.py
@@ -0,0 +1,69 @@
+"""
+Test for OWL 2 RL/RDF rules from
+
+    Table 8. The Semantics of Datatypes
+
+https://www.w3.org/TR/owl2-profiles/#Reasoning_in_OWL_2_RL_and_RDF_Graphs_using_Rules
+"""
+
+from rdflib import Graph, Literal, Namespace, RDF, XSD, RDFS
+
+import RDFClosure
+
+DAML = Namespace('http://www.daml.org/2002/03/agents/agent-ont#')
+T = Namespace('http://test.org/')
+
+def test_dt_type1():
+    """
+    Test dt-type1 for OWL 2 RL.
+    """
+    g = Graph()
+    RDFClosure.DeductiveClosure(RDFClosure.OWLRL_Semantics).expand(g)
+
+    assert (RDF.PlainLiteral, RDF.type, RDFS.Datatype) in g
+    assert (RDF.XMLLiteral, RDF.type, RDFS.Datatype) in g
+    assert (RDFS.Literal, RDF.type, RDFS.Datatype) in g
+    assert (XSD.decimal, RDF.type, RDFS.Datatype) in g
+    assert (XSD.integer, RDF.type, RDFS.Datatype) in g
+    assert (XSD.nonNegativeInteger, RDF.type, RDFS.Datatype) in g
+    assert (XSD.nonPositiveInteger, RDF.type, RDFS.Datatype) in g
+    assert (XSD.positiveInteger, RDF.type, RDFS.Datatype) in g
+    assert (XSD.negativeInteger, RDF.type, RDFS.Datatype) in g
+    assert (XSD.long, RDF.type, RDFS.Datatype) in g
+    assert (XSD.int, RDF.type, RDFS.Datatype) in g
+    assert (XSD.short, RDF.type, RDFS.Datatype) in g
+    assert (XSD.byte, RDF.type, RDFS.Datatype) in g
+    assert (XSD.unsignedLong, RDF.type, RDFS.Datatype) in g
+    assert (XSD.unsignedInt, RDF.type, RDFS.Datatype) in g
+    assert (XSD.unsignedShort, RDF.type, RDFS.Datatype) in g
+    assert (XSD.unsignedByte, RDF.type, RDFS.Datatype) in g
+    assert (XSD.float, RDF.type, RDFS.Datatype) in g
+    assert (XSD.double, RDF.type, RDFS.Datatype) in g
+    assert (XSD.string, RDF.type, RDFS.Datatype) in g
+    assert (XSD.normalizedString, RDF.type, RDFS.Datatype) in g
+    assert (XSD.token, RDF.type, RDFS.Datatype) in g
+    assert (XSD.language, RDF.type, RDFS.Datatype) in g
+    assert (XSD.Name, RDF.type, RDFS.Datatype) in g
+    assert (XSD.NCName, RDF.type, RDFS.Datatype) in g
+    assert (XSD.NMTOKEN, RDF.type, RDFS.Datatype) in g
+    assert (XSD.boolean, RDF.type, RDFS.Datatype) in g
+    assert (XSD.hexBinary, RDF.type, RDFS.Datatype) in g
+    assert (XSD.base64Binary, RDF.type, RDFS.Datatype) in g
+    assert (XSD.anyURI, RDF.type, RDFS.Datatype) in g
+    assert (XSD.dateTime, RDF.type, RDFS.Datatype) in g
+    assert (XSD.dateTimeStamp, RDF.type, RDFS.Datatype) in g
+    
+def test_dt_type2():
+    """
+    Test dt-type2 for OWL 2 RL.
+    """
+    g = Graph()
+    g.add((T.A, T.prop, Literal(-1, datatype=XSD.nonNegativeInteger)))
+    RDFClosure.DeductiveClosure(RDFClosure.OWLRL_Semantics).expand(g)
+
+    result = next(g.objects(predicate=DAML.error))
+    expected = Literal(
+        'Lexical value of the literal \'-1\' does not match its datatype'
+        ' (http://www.w3.org/2001/XMLSchema#nonNegativeInteger)'    
+    )
+    assert expected == result
