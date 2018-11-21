131296cf8667732d75f51e62f461a57bfd7a9721
Edmond Chuc <edmond.chuc@csiro.au>
2018-11-21 15:10:16 +1000

    Added RDFSClosure

--- RDFClosure/RDFSClosure.py
+++ RDFClosure/RDFSClosure.py
@@ -6,11 +6,21 @@ This module is brute force implementation of the RDFS semantics on the top of RD
 introductory text).
 
 
-@requires: U{RDFLib<https://github.com/RDFLib/rdflib>}, 4.0.0 and higher
-@license: This software is available for use under the U{W3C Software
-License<http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231>}
-@organization: U{World Wide Web Consortium<http://www.w3.org>}
-@author: U{Ivan Herman<a href="http://www.w3.org/People/Ivan/">}
+**Requires**: `RDFLib`_, 4.0.0 and higher.
+
+.. _RDFLib: https://github.com/RDFLib/rdflib
+
+**License**: This software is available for use under the `W3C Software License`_.
+
+.. _W3C Software License: http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231
+
+**Organization**: `World Wide Web Consortium`_
+
+.. _World Wide Web Consortium: http://www.w3.org
+
+**Author**: `Ivan Herman`_
+
+.. _Ivan Herman: http://www.w3.org/People/Ivan/
 
 """
 
@@ -35,15 +45,31 @@ from RDFClosure.AxiomaticTriples import RDFS_Axiomatic_Triples, RDFS_D_Axiomatic
 # RDFS Semantics class
 # noinspection PyPep8Naming
 class RDFS_Semantics(Core):
-    """RDFS Semantics class, ie, implementation of the RDFS closure graph.
+    """
+    RDFS Semantics class, ie, implementation of the RDFS closure graph.
 
-    Note that the module does I{not} implement the so called Datatype entailment rules, simply because the underlying
-    RDFLib does not implement the datatypes (ie, RDFLib will not make the literal "1.00" and "1.00000" identical,
-    although even with all the ambiguities on datatypes, this I{should} be made equal...). Also, the so-called
-    extensional entailment rules (Section 7.3.1 in the RDF Semantics document) have not been implemented either.
+    .. note:: Note that the module does *not* implement the so called Datatype entailment rules, simply because the
+        underlying RDFLib does not implement the datatypes (ie, RDFLib will not make the literal "1.00" and "1.00000"
+        identical, although even with all the ambiguities on datatypes, this I{should} be made equal...).
 
-    The comments and references to the various rule follow the names as used in the U{RDF Semantics
-    document<http://www.w3.org/TR/rdf-mt/>}.
+        Also, the so-called extensional entailment rules (Section 7.3.1 in the RDF Semantics document) have not been
+        implemented either.
+
+    The comments and references to the various rule follow the names as used in the `RDF Semantics document`_.
+
+    .. _RDF Semantics document: http://www.w3.org/TR/rdf-mt/
+
+    :param graph: The RDF graph to be extended.
+    :type graph: :class:`rdflib.Graph`
+
+    :param axioms: Whether (non-datatype) axiomatic triples should be added or not.
+    :type axioms: bool
+
+    :param daxioms: Whether datatype axiomatic triples should be added or not.
+    :type daxioms: bool
+
+    :param rdfs: Whether RDFS inference is also done (used in subclassed only).
+    :type rdfs: bool
     """
     def __init__(self, graph, axioms, daxioms, rdfs):
         """
@@ -86,12 +112,13 @@ class RDFS_Semantics(Core):
 
     # noinspection PyBroadException
     def one_time_rules(self):
-        """Some of the rules in the rule set are axiomatic in nature, meaning that they really have to be added only
+        """
+        Some of the rules in the rule set are axiomatic in nature, meaning that they really have to be added only
         once, there is no reason to add these in a cycle. These are performed by this method that is invoked only once
         at the beginning of the process.
 
         In this case this is related to a 'hidden' same as rules on literals with identical values (though different
-        lexical values)
+        lexical values).
         """
         # There is also a hidden sameAs rule in RDF Semantics: if a literal appears in a triple, and another one has
         # the same value, then the triple should be duplicated with the other value.
@@ -117,8 +144,12 @@ class RDFS_Semantics(Core):
     def rules(self, t, cycle_num):
         """
             Go through the RDFS entailment rules rdf1, rdfs4-rdfs12, by extending the graph.
-            @param t: a triple (in the form of a tuple)
-            @param cycle_num: which cycle are we in, starting with 1. Can be used for some (though minor) optimization.
+
+            :param t: A triple (in the form of a tuple).
+            :type t: tuple
+
+            :param cycle_num: Which cycle are we in, starting with 1. Can be used for some (though minor) optimization.
+            :type cycle_num: int
         """
         s, p, o = t
         # rdf1
