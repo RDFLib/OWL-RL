6e2266348650413bd444e6a9c28c077c78bc038f
Alex Nelson <alexander.nelson@nist.gov>
2022-02-11 17:19:22 -0500

    Cite expected behaviors of owl:Axioms and blank nodes
    A follow-on patch may be necessary to clarify closure behaviors.
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- test/test_owl_axiom.py
+++ test/test_owl_axiom.py
@@ -11,21 +11,41 @@
 #
 # We would appreciate acknowledgement if the software is used.
 
+"""
+This file tests behaviors of owl:Axioms.
+
+The OWL-RDF mapping [2], Table 7 row 7, would match the introduction
+pattern pertaining to EX.NamedAxiom with a Class Declaration, EXCEPT the
+axiom's identifier is an IRI instead of a blank node.  The document only
+defines behaviors for `owl:Axiom`s identifying as blank nodes.  Given
+this pattern non-match, the RDF graph will contain these triples after
+the mapping process of [2] Section 3 should have emptied the RDF graph.
+
+The requirement at the end of [2] Section 3, "At the end of this
+process, the graph G MUST be empty." fails, meaning [1] Section 2.1's
+requirement "conformant OWL 2 tools that take ontology documents as
+input(s) MUST accept ontology documents using the RDF/XML serialization"
+fails, placing the graph into OWL 2 FULL and providing no behaviors for
+or against constructing a class declaration.
+
+[1] https://www.w3.org/TR/2012/REC-owl2-conformance-20121211/
+[2] https://www.w3.org/TR/2012/REC-owl2-mapping-to-rdf-20121211/
+"""
+
+import logging
 from pathlib import Path
 
 import owlrl
+import pytest
 from rdflib import BNode, Graph, Namespace
 from rdflib.namespace import OWL, RDF
 
 EX = Namespace("http://example.org/")
 RELS = Namespace("http://example.org/relatives#")
 
-def test_owl_axiom() -> None:
+def _test_owl_axiom(include_named_axiom: bool) -> None:
     """
     Test whether an axiom written only as an owl:Axiom is an asserted axiom.
-
-    https://www.w3.org/TR/2012/REC-owl2-mapping-to-rdf-20121211/
-    Table: 7, row 7
     """
     g = Graph()
     relatives_ttl_path = Path(__file__).parent / "relatives.ttl"
@@ -37,29 +57,45 @@ def test_owl_axiom() -> None:
     g.add((EX.MaxByAssertion, RDF.type, RELS.Person))
 
     # Second, Max, not directly asserted, but would be asserted if this blank-node axiom were not reified.
-    # NOTE - The OWL-RDF mapping, Table 7 row 7, matches this pattern with a Class Declaration.
+    # NOTE - [2] Table 7 row 7 matches this pattern with a Class Declaration.
     ax = BNode()
     g.add((ax, RDF.type, OWL.Axiom))
     g.add((ax, OWL.annotatedSource, EX.MaxByBlankAxiom))
     g.add((ax, OWL.annotatedProperty, RDF.type))
     g.add((ax, OWL.annotatedTarget, RELS.Person))
 
-    # Third, Max, not directly asserted, but would be asserted if this named axiom were not reified.
-    # NOTE - The OWL-RDF mapping, Table 7 row 7, matches this pattern with a Class Declaration, EXCEPT the node is an IRI instead of a blank node.  This may be undefined behavior.
-    g.add((EX.NamedAxiom, RDF.type, OWL.Axiom))
-    g.add((EX.NamedAxiom, OWL.annotatedSource, EX.MaxByNamedAxiom))
-    g.add((EX.NamedAxiom, OWL.annotatedProperty, RDF.type))
-    g.add((EX.NamedAxiom, OWL.annotatedTarget, RELS.Person))
+    if include_named_axiom:
+        # Third, Max, not directly asserted, but would be asserted if this named axiom were not reified.
+        # NOTE - [2] Table 7 row 7 does not match this pattern with a Class Declaration.
+        g.add((EX.NamedAxiom, RDF.type, OWL.Axiom))
+        g.add((EX.NamedAxiom, OWL.annotatedSource, EX.MaxByNamedAxiom))
+        g.add((EX.NamedAxiom, OWL.annotatedProperty, RDF.type))
+        g.add((EX.NamedAxiom, OWL.annotatedTarget, RELS.Person))
 
     # Before defining and expanding closure, confirm the direct and both reified Maxes are, are not, and are not in the graph, respectively.
     assert (EX.MaxByAssertion, RDF.type, RELS.Person) in g
     assert not (EX.MaxByBlankAxiom, RDF.type, RELS.Person) in g
     assert not (EX.MaxByNamedAxiom, RDF.type, RELS.Person) in g
 
-    owlrl.DeductiveClosure(owlrl.OWLRL_Extension).expand(g)
+    try:
+        owlrl.DeductiveClosure(owlrl.OWLRL_Extension).expand(g)
+    except:
+        if include_named_axiom:
+            pytest.xfail("The OWL 2 RL closure correctly rejected an IRI-identified owl:Axiom.")
+        else:
+            raise
 
-    # After defining and expanding closure, confirm the direct and reified Maxes are in the graph according to OWL-RDF mapping specification.
+    if include_named_axiom:
+        assert False, "The OWL 2 RL closure should have rejected an IRI-identified owl:Axiom."
+
+    # After defining and expanding closure, confirm the direct and reified Maxes are in the graph according to [2].
     assert (EX.MaxByAssertion, RDF.type, RELS.Person) in g
     assert (EX.MaxByBlankAxiom, RDF.type, RELS.Person) in g
-    # TODO - This last behavior might be undefined.
-    assert not (EX.MaxByNamedAxiom, RDF.type, RELS.Person) in g
+
+
+def test_owl_axiom_blank() -> None:
+    _test_owl_axiom(False)
+
+
+def test_owl_axiom_iri() -> None:
+    _test_owl_axiom(True)
